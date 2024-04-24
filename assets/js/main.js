393e759be09a60b3c6d3edf7ffa64971edfd1ed4
sorin04 <sorin.pirgari@lyceeastier.com>
2024-04-24 19:41:57 +0200

    5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,7 @@
-// Fonction pour charger une image dynamique
-function loadDynamicImage(imageURL) {
-    // Sélectionner l'élément du carousel pour l'image dynamique
-    var dynamicImageItem = document.querySelector("#dynamicImage");
-    // Mettre à jour l'URL de l'image
-    dynamicImageItem.style.backgroundImage = "url(" +tirana.jpg+ ")";
+console.log("test js");
+document.onreadystatechange = function () {
+    console.log(document.readyState)
+    if(document.readyState == 'interactive') {
+        console.log("c'est ok")
+    }
 }
-
-// Appeler la fonction pour charger une image dynamique
-var dynamicImageURL = "assets/img/tirana.jpg"; // Remplacez "assets/img/s.jpg" par l'URL de votre image dynamique
-loadDynamicImage(dynamicImageURL);
